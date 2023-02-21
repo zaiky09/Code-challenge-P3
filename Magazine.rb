6d1721f622f05ec110377909e1989b4adc7df331
zaiky09 <zamilmozamil@outlook.com>
2023-02-21 14:55:17 +0300

    Correctly initialized Magazine

--- Magazine.rb
+++ Magazine.rb
@@ -2,28 +2,41 @@
 
 class Magazine
 
-    attr_accessor :name, :category :author, :magazine, :title
+    attr_accessor :name, :category
     
+    @@all = []
+
     def initialize (name, category)
         @name = name
         @category = category
+        @@all << self 
     end
-    
-    a = Magazine.new("Code-101", "Progamming")
-    puts a.name + a.category
 
-    # attr_accessor :author, :magazine, :title
-        def self.all
-            rows = DB.execute(SELECT * FROM articles)
-            rows.map do |row|
-            self.new(row)
-            end
-        end
-    def initialize (row)
-        @id = row["id"]
-        @author = author["author"]
-        @magazine = magazine["magazine"]
-        @title = title["title"]
+    def self.all
+        @@all
     end
+
+    mag1 = Magazine.new("Speed", "Cars")
+    mag2 = Magazine.new("Cribs", "Houses")
+    puts mag1.name, + mag1.category
+ 
+
+
+    # a = Magazine.new("Code-101", "Progamming")
+    # puts a.name + a.category
+
+    # # attr_accessor :author, :magazine, :title
+    #     def self.all
+    #         rows = DB.execute(SELECT * FROM articles)
+    #         rows.map do |row|
+    #         self.new(row)
+    #         end
+    #     end
+    # def initialize (row)
+    #     @id = row["id"]
+    #     @author = author["author"]
+    #     @magazine = magazine["magazine"]
+    #     @title = title["title"]
+    # end
 
 end
